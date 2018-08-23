90b46b77a88750ba303df82e5bffccc7a8cb1585
George Merrill <gm.webdev0@gmail.com>
2018-08-23 19:37:11 -0400

    The gingerbread man

---
+++ Case_Study_2_2.py
@@ -0,0 +1,55 @@
+# Examples and Exercises From 'Computer Graphics using Open GL" by Hill 2nd Ed
+# all code by George A. Merrill (except where otherwise noted)
+#################################################################################################
+# Case Study 2.1 The gingerbread man
+import sys
+import OpenGL
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+screenWidth = 640
+screenHeight = 480
+
+def myInit():
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glColor3f(1.0, 1.0, 1.0)
+    glPointSize(1.0)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0.0, screenWidth, 0.0, screenHeight)
+
+
+def myDisplay():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glBegin(GL_POINTS)
+    px = 115
+    py = 121
+    for i in range(270000):
+        glVertex2i(px, screenHeight - py)
+        qy = px
+        qx = 40 * (1 + 2 * 3) - py + abs(px - 120)
+        px = qx
+        py = qy
+    glEnd()
+    glFlush()
+
+def main():
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    glutInitWindowSize(screenWidth, screenHeight)
+    glutInitWindowPosition(100, 50)
+    glutCreateWindow(b'The gingerbread man')
+
+    # register the callback functions
+    glutDisplayFunc(myDisplay)
+    #    glutReshapeFunc(myReshape)
+    #    glutMouseFunc(myMouse)
+    #    glutKeyboardFunc(myKeyboard)
+    #    glutMotionFunc(myMove)
+
+    myInit()
+    glutMainLoop()
+
+
+main()
